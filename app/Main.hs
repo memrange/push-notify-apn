ffa4823e77d94ad4169058a22a9997c4d12bfc30
Hans-Christian Esperer <hc@hcesperer.org>
2017-09-09 11:22:21 +0200

    Add interactive mode

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,26 @@ module Main where
 
 import Control.Concurrent
 import Control.Monad
+import Data.Maybe
 import Data.Semigroup ((<>))
 import Options.Applicative
 
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.Text as T
+import qualified Data.Text.IO as TI
 
 import Network.PushNotify.APN
 
 
 data ApnOptions = ApnOptions
-  { certpath :: !String
-  , keypath  :: !String
-  , capath   :: !String
-  , topic    :: !String
-  , tokens   :: !([String])
-  , sandbox  :: !Bool
-  , text     :: !String }
+  { certpath    :: !String
+  , keypath     :: !String
+  , capath      :: !String
+  , topic       :: !String
+  , tokens      :: !([String])
+  , sandbox     :: !Bool
+  , interactive :: !Bool
+  , text        :: !(Maybe String) }
 
 p :: Parser ApnOptions
 p = ApnOptions
@@ -47,10 +50,14 @@ p = ApnOptions
           ( long "sandbox"
          <> short 's'
          <> help "Whether to use the sandbox (non-production deployments)" )
-      <*> strOption
+      <*> switch
+          ( long "interactive"
+         <> short 'i'
+         <> help "Run an interactive mode; each line must have this format: token:message " )
+      <*> optional (strOption
           ( short 'm'
          <> metavar "MESSAGE"
-         <> help "Message to send to the device" )
+         <> help "Message to send to the device" ))
       
 
 main :: IO ()
@@ -64,8 +71,23 @@ main = send =<< execParser opts
 send :: ApnOptions -> IO ()
 send o = do
     session <- newSession (keypath o) (certpath o) (capath o) (sandbox o) 10 (B8.pack $ topic o)
-    let payload  = alertMessage "push-notify-apn" (T.pack $ text o)
-        message  = newMessage payload
-    forM_ (tokens o) $ \token ->
-        let apntoken = hexEncodedToken . T.pack $ token
-        in sendMessage session apntoken message >>= print
+    if interactive o
+    then forever $ do
+        TI.putStrLn "Message in the form token:message please"
+        line <- TI.getLine
+        let parts = T.splitOn ":" line
+        if length parts >= 2
+        then
+            let token   = hexEncodedToken $ head parts
+                text    = T.intercalate ":" (tail parts)
+                payload = alertMessage "push-notify-apn" text
+                message = newMessage payload
+            in sendMessage session token message >>= TI.putStrLn . T.pack . show
+        else TI.putStrLn "Erroneous format"
+    else do
+        when (isNothing $ text o) $ error "You need to specify a message with -m"
+        let payload  = alertMessage "push-notify-apn" (T.pack $ fromJust $ text o)
+            message  = newMessage payload
+        forM_ (tokens o) $ \token ->
+            let apntoken = hexEncodedToken . T.pack $ token
+            in sendMessage session apntoken message >>= print
