41dd269e43076b8dffbd1afacc473954272d8c3b
Hans-Christian Esperer <hc@hcesperer.org>
2018-12-09 13:57:32 +0100

    Remove hspec-discover for now

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,8 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+-- {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+
+import Test.Hspec
+
+main = hspec $
+  describe "Network.PushNotify.APN" $ do
+    it "should run a test suite" $ do
+      1 `shouldBe` 1
