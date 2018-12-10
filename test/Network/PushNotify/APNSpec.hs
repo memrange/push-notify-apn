ea21c7f58b2b4b00b9831cf8c3c6e11cad365ccd
Jesse Kempf <jesse.kempf@binarysunrise.io>
2018-12-10 14:15:57 -0800

    Accept supplemental fields for the notification payload

--- test/Network/PushNotify/APNSpec.hs
+++ test/Network/PushNotify/APNSpec.hs
@@ -7,7 +7,7 @@ import           Network.PushNotify.APN
 import           Test.Hspec
 
 spec :: Spec
-spec =
+spec = do
   describe "JsonApsMessage" $
     context "JSON encoder" $ do
       it "encodes an APNS message with a title and body" $
@@ -29,3 +29,26 @@ spec =
             "badge"    .= Null,
             "alert"    .= object [ "body"  .= String "hello world" ]
           ]
+
+  describe "JsonAps" $
+    context "JSON encoder" $ do
+      it "encodes normally when there are no supplemental fields" $
+        toJSON (newMessage (alertMessage "hello" "world")) `shouldBe` object [
+          "aps"                .= alertMessage "hello" "world",
+          "appspecificcontent" .= Null
+        ]
+
+      it "encodes supplemental fields" $ do
+        let msg = newMessage (alertMessage "hello" "world")
+                  & addSupplementalField "foo" ("bar" :: String)
+                  & addSupplementalField "aaa" ("qux" :: String)
+
+        toJSON msg `shouldBe` object [
+            "aaa"                .= String "qux",
+            "aps"                .= alertMessage "hello" "world",
+            "appspecificcontent" .= Null,
+            "foo"                .= String "bar"
+          ]
+
+  where
+    (&) = flip ($)
