411de61a171e511c63545dfbbad53e8502b3bd11
Hans-Christian Esperer <hc@hcesperer.org>
2018-12-11 18:48:31 +0100

    Revert "Remove hspec-discover for now"
    This reverts commit 41dd269e43076b8dffbd1afacc473954272d8c3b.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,1 @@
--- {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
-
-import Test.Hspec
-
-main = hspec $
-  describe "Network.PushNotify.APN" $ do
-    it "should run a test suite" $ do
-      1 `shouldBe` 1
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
